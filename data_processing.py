ad494b3bced27999df95b076687ee513e13c00bd
Lisa Xie <lisaxie3001@gmail.com>
2022-11-17 14:06:28 -0500

    Functions to convert angles to distance

--- data_processing.py
+++ data_processing.py
@@ -1,38 +1,53 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 min = 110
 max = 200
 
 df = pd.read_csv('data.csv')
 
-interval = []
-intervalColors = colors = {'1':'red', '2':'yellow', '3':'green', '4':'blue'}
+def getDistance(x, y , distance): 
+    new_distance = distance
+    return new_distance;
 
-def getInterval(distance: int):
-    temp = 0
-    if (distance < min + 5):
-        temp = 1
-    elif (distance < min + 10):
-        temp = 2
-    elif (distance < min + 15):
-        temp = 3
-    else:
-        temp = 4
-    return temp
+def getX(x):
+    return x
 
-# filter range
+def getY(y):
+    return y
+
 for x in df.index:
-    if df.loc[x, "Distance"] < min or df.loc[x, "Distance"] > max:
+    cur_distance = getDistance(df.loc[x, 'x'], df.loc[x, 'y'], df.loc[x, 'Distance'])
+    
+    # filter range
+    if cur_distance < min or cur_distance > max:
         df.drop(x, inplace = True)
+    
+    # convert from angle to distance
     else:
-        interval.append(getInterval(df.loc[x, "Distance"]))
-
-# add intervals to dataframe
-df['Interval'] = interval
+        df.loc[x, 'x'] = getX(df.loc[x, 'x'])
+        df.loc[x, 'y'] = getY(df.loc[x, 'y'])
+        df.loc[x, 'Distance'] = cur_distance
 
 df.plot(kind = 'scatter', x = 'x', y = 'y', c = 'Distance', colormap= 'viridis')
 plt.show()
 
-print(df.to_string())
+
+
+# interval = []
+# intervalColors = colors = {'1':'red', '2':'yellow', '3':'green', '4':'blue'}
+
+# def getInterval(distance: int):
+#     temp = 0
+#     if (distance < min + 5):
+#         temp = 1
+#     elif (distance < min + 10):
+#         temp = 2
+#     elif (distance < min + 15):
+#         temp = 3
+#     else:
+#         temp = 4
+#     return temp
+
+# # add intervals to dataframe
+# df['Interval'] = interval
