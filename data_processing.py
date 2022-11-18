bdadfe825c20e1e1fe08252f0ed4bd757a2338d3
Lisa Xie <lisaxie3001@gmail.com>
2022-11-18 00:46:43 -0500

    Nov 18 iteration + data

--- data_processing.py
+++ data_processing.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-min = 110
-max = 200
+min = 10 #37
+max = 50 #47
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('rock1.csv')
+df['Distance'] = pd.to_numeric(df['Distance'])
 
 def getDistance(x, y , distance): 
     new_distance = distance
-    return new_distance;
+    return new_distance
 
 def getX(x):
     return x
@@ -18,7 +19,7 @@ def getY(y):
 
 for x in df.index:
     cur_distance = getDistance(df.loc[x, 'x'], df.loc[x, 'y'], df.loc[x, 'Distance'])
-    
+
     # filter range
     if cur_distance < min or cur_distance > max:
         df.drop(x, inplace = True)
@@ -30,6 +31,9 @@ for x in df.index:
         df.loc[x, 'Distance'] = cur_distance
 
 df.plot(kind = 'scatter', x = 'x', y = 'y', c = 'Distance', colormap= 'viridis')
+
+plt.xlim([-20,20])
+plt.ylim([-2,34])
 plt.show()
 
 
