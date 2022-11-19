70c01b6a3e3eec1b1c40ecccf5a59d6ded53c151
Lisa Xie <lisaxie3001@gmail.com>
2022-11-19 02:55:15 -0500

    Remove unused functions

--- data_processing.py
+++ data_processing.py
@@ -15,12 +15,6 @@ def getValues(x, y , distance):
     values.append(distance * math.cos(math.radians(y)) * math.cos(math.radians(x)))
     return values
 
-def getX(x):
-    return x
-
-def getY(y):
-    return y
-
 for x in df.index:
     values = getValues(df.loc[x, 'x'], df.loc[x, 'y'], df.loc[x, 'Distance'])
 
@@ -39,23 +33,3 @@ df.plot(kind = 'scatter', x = 'x', y = 'y', c = 'Distance', colormap= 'viridis')
 plt.xlim([-20,20])
 plt.ylim([-2,34])
 plt.show()
-
-
-
-# interval = []
-# intervalColors = colors = {'1':'red', '2':'yellow', '3':'green', '4':'blue'}
-
-# def getInterval(distance: int):
-#     temp = 0
-#     if (distance < min + 5):
-#         temp = 1
-#     elif (distance < min + 10):
-#         temp = 2
-#     elif (distance < min + 15):
-#         temp = 3
-#     else:
-#         temp = 4
-#     return temp
-
-# # add intervals to dataframe
-# df['Interval'] = interval
