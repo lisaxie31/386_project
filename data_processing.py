bb601fdb951c3d655efcd916903f8878a2385408
Lisa Xie <lisaxie3001@gmail.com>
2022-11-24 02:34:54 -0500

    save processed data to csv + cleanup

--- data_processing.py
+++ data_processing.py
@@ -7,9 +7,6 @@ min = 0 #37
 max = 25 #47
 
 stepAngle = 3
-ser = serial.Serial('/dev/cu.usbserial-10')
-# df = pd.read_csv('test.csv')
-dfTest = pd.DataFrame(columns=['x', 'y', 'Distance'])
 
 def getValues(x, y , distance): 
     values = []
@@ -21,34 +18,39 @@ def getValues(x, y , distance):
     values.append(distance * math.cos(math.radians(y)) * math.cos(math.radians(x)))
     return values
 
-while(ser.isOpen()):
-    line = ser.readline().decode("utf-8").rstrip()
-    print(line)
-    if line == 'end':
-        break
-    line = line.split(',')
+# ser = serial.Serial('/dev/cu.usbserial-10')
+# df = pd.DataFrame(columns=['x', 'y', 'Distance'])
+# while(ser.isOpen()):
+#     line = ser.readline().decode("utf-8").rstrip()
+#     print(line)
+
+#     if line == 'end':
+#         break
+#     line = line.split(',')
+#     values = getValues(float(line[0]) * stepAngle, float(line[1]) * stepAngle, float(line[2]))
     
-    values = getValues(float(line[0]) * stepAngle, float(line[1]) * stepAngle, float(line[2]))
-    if values[2] > min and values[2] < max:
-        dfTest.loc[len(dfTest.index)] = values
+#     if values[2] > min and values[2] < max:
+#         df.loc[len(df.index)] = values
+# print("connection closed")
+
+df = pd.read_csv('test.csv')
+for x in df.index:
+    values = getValues(stepAngle* df.loc[x, 'x'], stepAngle * df.loc[x, 'y'], df.loc[x, 'Distance'])
+
+    # filter range
+    if values[2] < min or values[2] > max:
+        df.drop(x, inplace = True)
     
-print("connection closed")
+    # convert from angle to distance
+    else:
+        df.loc[x, 'x'] = values[0]
+        df.loc[x, 'y'] = values[1]
+        df.loc[x, 'Distance'] = values[2]
 
-# for x in df.index:
-#     values = getValues(stepAngle* df.loc[x, 'x'], stepAngle * df.loc[x, 'y'], df.loc[x, 'Distance'])
 
-#     # filter range
-#     if values[2] < min or values[2] > max:
-#         df.drop(x, inplace = True)
-    
-#     # convert from angle to distance
-#     else:
-#         df.loc[x, 'x'] = values[0]
-#         df.loc[x, 'y'] = values[1]
-#         df.loc[x, 'Distance'] = values[2]
+df.to_csv('processed_data.csv', encoding='utf-8', index=False)
+df.plot(kind = 'scatter', x = 'x', y = 'y', c = 'Distance', colormap= 'viridis')
 
-dfTest.plot(kind = 'scatter', x = 'x', y = 'y', c = 'Distance', colormap= 'viridis')
-
-plt.xlim([-15,15])
-plt.ylim([-2,20])
+# plt.xlim([-40,40])
+# plt.ylim([-2,50])
 plt.show()
