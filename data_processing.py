0476f62f7e4a5d93294b49e8e5474719d4ae0081
Lisa Xie <lisaxie3001@gmail.com>
2022-11-24 19:05:26 -0500

    new trials

--- data_processing.py
+++ data_processing.py
@@ -18,39 +18,42 @@ def getValues(x, y , distance):
     values.append(distance * math.cos(math.radians(y)) * math.cos(math.radians(x)))
     return values
 
-# ser = serial.Serial('/dev/cu.usbserial-10')
-# df = pd.DataFrame(columns=['x', 'y', 'Distance'])
-# while(ser.isOpen()):
-#     line = ser.readline().decode("utf-8").rstrip()
-#     print(line)
+collectData = 0
+if collectData == 1: 
+    ser = serial.Serial('/dev/cu.usbserial-10')
+    df = pd.DataFrame(columns=['x', 'y', 'Distance'])
+    while(ser.isOpen()):
+        line = ser.readline().decode("utf-8").rstrip()
+        print(line)
 
-#     if line == 'end':
-#         break
-#     line = line.split(',')
-#     values = getValues(float(line[0]) * stepAngle, float(line[1]) * stepAngle, float(line[2]))
-    
-#     if values[2] > min and values[2] < max:
-#         df.loc[len(df.index)] = values
-# print("connection closed")
+        if line == 'end':
+            break
+        line = line.split(',')
+        values = getValues(float(line[0]) * stepAngle, float(line[1]) * stepAngle, float(line[2]))
+        
+        if values[2] > min and values[2] < max:
+            df.loc[len(df.index)] = values
+    print("connection closed")
 
-df = pd.read_csv('test.csv')
-for x in df.index:
-    values = getValues(stepAngle* df.loc[x, 'x'], stepAngle * df.loc[x, 'y'], df.loc[x, 'Distance'])
+else:
+    df = pd.read_csv('trials/L4.csv')
+    for x in df.index:
+        values = getValues(stepAngle* df.loc[x, 'x'], stepAngle * df.loc[x, 'y'], df.loc[x, 'Distance'])
 
-    # filter range
-    if values[2] < min or values[2] > max:
-        df.drop(x, inplace = True)
-    
-    # convert from angle to distance
-    else:
-        df.loc[x, 'x'] = values[0]
-        df.loc[x, 'y'] = values[1]
-        df.loc[x, 'Distance'] = values[2]
+        # filter range
+        if values[2] < min or values[2] > max:
+            df.drop(x, inplace = True)
+        
+        # convert from angle to distance
+        else:
+            df.loc[x, 'x'] = values[0]
+            df.loc[x, 'y'] = values[1]
+            df.loc[x, 'Distance'] = values[2]
 
 
 df.to_csv('processed_data.csv', encoding='utf-8', index=False)
 df.plot(kind = 'scatter', x = 'x', y = 'y', c = 'Distance', colormap= 'viridis')
 
-# plt.xlim([-40,40])
-# plt.ylim([-2,50])
+plt.xlim([-10,10])
+plt.ylim([-1,10])
 plt.show()
